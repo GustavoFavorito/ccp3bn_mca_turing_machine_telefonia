afc4cbe51cc98c6073bcce3448e91c4f7f6388df
unknown <gustavofavorito.d@gmail.com>
2019-09-30 20:10:34 -0300

    pegando elementos da div e splitando

--- src/js/turing-machine.js
+++ src/js/turing-machine.js
@@ -1,3 +1,6 @@
 function turingMachine() {
-    var a = document.getElementById('modal-output').innerHTML = 'Funciounou!';
+    var output = document.getElementById('output').innerHTML;
+    var parse = output.split(/<\/?span>/).filter(Boolean);
+    console.log(parse);
+    document.getElementById('modal-output').innerHTML = output;
 };
